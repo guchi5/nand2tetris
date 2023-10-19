34d4c4c6ec4711207a296f9731376572f1bdf260
guchi5 <zero19390901@gmail.com>
2023-10-19 21:32:19 +0900

    VM変換器の定義 - Parser.java -> 一つのvmファイルに対してパースを行うとともに、入力コードへのアクセスをカプセル化する - CodeWriter.java -> VMコマンドをアセンブリコードに変換する - Main.java -> ParserとCodeWriterモジュール用いてアセンブリコードへの変換と出力ファイルへの書き込みを行う

---
+++ projects/vm_transducter/CodeWrite.java
@@ -0,0 +1,23 @@
+public class CodeWrite {
+    public CodeWrite(){
+
+    }
+
+    public void setFileName(){
+
+    }
+
+    public void writeArithmetic(){
+
+    }
+
+    public void writePushPop(){
+
+    }
+
+    public void close(){
+
+    }
+
+    
+}
---
+++ projects/vm_transducter/Parser.java
@@ -0,0 +1,22 @@
+public class Parser {
+    public Parser(){
+
+    }
+    
+    public boolean hasMoreCommands(){
+        return true;
+    }
+
+    public String commandType(){
+        return null;
+    }
+
+    public String arg1(){
+        return null;
+    }
+
+    public int arg2(){
+        return 0;
+    }
+
+}
